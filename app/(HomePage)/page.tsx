9367d024a2a23d8e7e2fcb210761c83607b642fa
dongdong867 <dongdong0867@gmail.com>
2023-03-31 03:20:40 +0800

    feat(file construction): move home to (HomePage)

---
+++ app/(HomePage)/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { getAccessToken } from '../libs/getAccessToken'
+import { getTasks } from '../libs/getTasks'
+
+export default async function Home({
+	searchParams
+}: {
+	searchParams?: { [key: string]: string | undefined }
+}) {
+	const code = searchParams?.code
+	if (code != null) {
+		const accesstokenData: Promise<TokenData> = getAccessToken(code)
+		const accesstoken = await accesstokenData
+
+		const taskData: Promise<Task[]> = getTasks(accesstoken, 1)
+		const tasks = await taskData
+
+		return tasks.map((task) => (
+			<>
+				<div>task id: {task.id}</div>
+				<div>task node_id: {task.node_id}</div>
+				<div>task number: {task.number}</div>
+				<div>task state: {task.state}</div>
+				<div>task title: {task.title}</div>
+				<div>task body: {task.body}</div>
+				<div>task labels: {task.labels.map((label) => label.name)}</div>
+			</>
+		))
+	}
+
+	return <div>test</div>
+}
