d8cdc0d1da1e3fc3af6e622042c6e2a690a7bfb5
dongdong867 <dongdong0867@gmail.com>
2023-03-31 21:32:09 +0800

    feat(font): import montserrat font to tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
+import './global.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+	variable: '--font-montserrat',
+	display: 'swap'
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang='en'>
+		<html lang='en' className={`${montserrat.variable}`}>
 			<body>{children}</body>
 		</html>
 	)
