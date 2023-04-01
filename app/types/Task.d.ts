7eaffc0ee7d638732a9a7d425aed832be85b30d9
dongdong867 <dongdong0867@gmail.com>
2023-04-02 06:33:28 +0800

    feat(api): add create task data

--- app/types/Task.d.ts
+++ app/types/Task.d.ts
@@ -1,20 +1,19 @@
 type Task = {
-  id: number
-  node_id: string
-  number: number
-  state: string
-  title: string
-  body: string
-  labels: Label[]
-}
-
-type SendTaskData = {
-  number?: number
-  title: string
-  body: string
-  labels: Label[]
+	id: number
+	node_id: string
+	number: number
+	state: string
+	title: string
+	body: string
+	labels: Label[]
 }
 
 type Label = {
-  name: 'open' | 'in progress' | 'done'
+	name: 'open' | 'in progress' | 'done'
+}
+
+type CreateTaskData = {
+	title: string
+	body: string
+	labels: Label[]
 }
