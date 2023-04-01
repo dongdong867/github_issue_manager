e4d86190b896cedabacbd648c3ccdca4f6471c6f
dongdong867 <dongdong0867@gmail.com>
2023-04-02 06:40:51 +0800

    feat(api): add create tasks page with api caller

---
+++ app/create/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CreateButton from './components/CreateButton'
+
+const CreateTaskPage = () => {
+	return (
+		<>
+			<div>Create Task Page</div>
+			<CreateButton />
+		</>
+	)
+}
+
+export default CreateTaskPage
