b6568eb7cc35a3e94e54fae71f234ba8e494a57e
dongdong867 <dongdong0867@gmail.com>
2023-04-02 19:27:51 +0800

    feat(api): implement search and display tasks in home

--- app/(HomePage)/page.tsx
+++ app/(HomePage)/page.tsx
@@ -1,12 +1,43 @@
-import React from 'react'
-import Tasks from './components/Tasks'
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import Task from './components/Task'
+import Search from './components/Search'
 
 export const revalidate = 0
 
-const Home = async () => {
+const Home = () => {
+	const [tasks, setTasks] = useState([] as Task[])
+	const [page, setPage] = useState(1)
+	const [isSearching, setIsSearching] = useState(false)
+	const [isLoading, setLoading] = useState(false)
+
+	useEffect(() => {
+		setLoading(true)
+		fetch(`/api/getTasks?page=${page}`, {
+			cache: 'no-store'
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setTasks(data)
+				setLoading(false)
+			})
+	}, [])
+
+	if (isLoading) return <div>Loading</div>
+	if (!tasks) return <div>No data</div>
+
 	return (
 		<div>
-			<Tasks />
+			<Search
+				page={page}
+				isSearching={isSearching}
+				setIsSearching={setIsSearching}
+				setTasks={setTasks}
+			/>
+			{tasks.map((task) => (
+				<Task key={task.number} task={task} />
+			))}
 		</div>
 	)
 }
