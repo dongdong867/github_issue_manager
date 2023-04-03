095c240ff46fc85db23381e520750133c22af92a
dongdong867 <dongdong0867@gmail.com>
2023-04-03 15:50:00 +0800

    feat(tailwind): add config and basic settings to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./app/**/*.{js,ts,jsx,tsx}'],
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'
+	],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['var(--font-montserrat)']
+				sans: ['var(--font-Montserrat)']
+			},
+			colors: {
+				primary: '#4285f4',
+				'primary-content-light': '#f5f5f5',
+				'primary-content-dark': '#202328',
+				button: '#16c984',
+				'base-light': '#eaeaea',
+				'base-dark': '#1f1f1f',
+				'border-primary': '#7188B4',
+				open: '#4285f4',
+				'in-process': '#ce5e5e',
+				done: '#16c984'
+			},
+			borderRadius: {
+				primary: '20px',
+				small: '10px'
+			},
+			boxShadow: {
+				base: '-5px -5px 15px #fcfcfc, 6px 6px 18px -14px #878787, 7px 6px 7px -6px rgba(0, 0, 0, 0.11)'
 			}
 		}
 	},
