0fba6a172d4c09eb1209d4374094eb1da0eb2a15
dongdong867 <dongdong0867@gmail.com>
2023-04-04 02:43:03 +0800

    style(label): remove unused code comments

--- app/(HomePage)/components/Label.tsx
+++ app/(HomePage)/components/Label.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { getLabelColor } from '@/app/lib/getLabelColor'
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
 
 type Params = {
 	label: string
@@ -14,6 +12,7 @@ const Label = ({ label, route, setLabel }: Params) => {
 	let primaryColor = ''
 	let bgColor = ''
 	let borderColor = getLabelColor(label)
+
 	switch (route) {
 		case 'task':
 			primaryColor = 'base-dark'
@@ -23,19 +22,7 @@ const Label = ({ label, route, setLabel }: Params) => {
 			primaryColor = 'base-light'
 			bgColor = 'base-dark'
 	}
-	// switch (label) {
-	// 	case 'open':
-	// 		borderColor = 'open'
-	// 		break
-	// 	case 'in_progress':
-	// 		borderColor = 'in-progress'
-	// 		break
-	// 	case 'done':
-	// 		borderColor = 'done'
-	// 		break
-	// 	default:
-	// 		borderColor = primaryColor
-	// }
+
 	return (
 		<div
 			className={`rounded-small w-max border-${borderColor} border-2 overflow-hidden p-1 shrink-0 font-medium`}
